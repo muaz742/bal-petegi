74ab00eeb08bea881b4a6c2c36691637d0f12a94
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2020-05-11 00:54:24 +0300

    kayıt ekle işlevi tanımlandı

--- control.php
+++ control.php
@@ -3,4 +3,27 @@
 # ön yükleme komutları
 require_once "vendor/autoload.php";
 
+# model fonksiyonları yükle
+require_once "model.php";
+
+if (p('gonder')=='kayit'){
+    $ePosta= p('e_posta');
+    $adi= p('adi');
+    $soyadi= p('soyadi');
+    $dogumYili= p('dogum_yili');
+    $sehir= p('sehir');
+    $parola= p('parola');
+    $parola = base64_encode($parola);
+    /*print_r($ePosta);
+    print_r($adi);
+    print_r($soyadi);
+    print_r($dogumYili);
+    print_r($sehir);
+    print_r($parola);
+    exit();*/
+
+    kayitEkle($ePosta,$adi,$soyadi,$dogumYili,$sehir,$parola);
+    exit();
+}
+
 header("Location: https://".$_SERVER['HTTP_HOST']);
--- model.php
+++ model.php
@@ -3,4 +3,38 @@
 # ön yükleme komutları
 require_once "vendor/autoload.php";
 
-header("Location: https://".$_SERVER['HTTP_HOST']);
+# kullanıcı kaydı ekleme fonksiyonu
+function kayitEkle($ePosta, $adi, $soyadi, $dogumYili, $sehir, $parola)
+{
+    global $db;
+    $sorgu = $db->prepare('INSERT INTO `kisiler` 
+                                    (
+                                    `e_posta`, 
+                                    `adi`, 
+                                    `soyadi`, 
+                                    `dogum_yili`, 
+                                    `sehir`, 
+                                    `parola`, 
+                                    `zaman`
+                                    ) 
+                                    VALUES 
+                                    (?, ?, ?, ?, ?, ?, ?)
+        ');
+    $zaman = time();
+    try {
+        $calistir = $sorgu->execute([
+            $ePosta,
+            $adi,
+            $soyadi,
+            $dogumYili,
+            $sehir,
+            $parola,
+            $zaman
+        ]);
+        ($calistir)?header("Location: /index?durum=basarili"):null;
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+//header("Location: https://".$_SERVER['HTTP_HOST']);
